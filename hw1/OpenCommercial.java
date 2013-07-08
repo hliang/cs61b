6ddb848e51d181595a944177df47469b60beeeed
H Liang <hliang@ksu>
2013-07-07 22:27:29 -0500

    finish hw 1 problem 1

--- hw1/OpenCommercial.java
+++ hw1/OpenCommercial.java
@@ -2,6 +2,7 @@
 
 import java.net.*;
 import java.io.*;
+import java.util.Arrays;
 
 /**  A class that provides a main function to read five lines of a commercial
  *   Web page and print them in reverse order, given the name of a company.
@@ -28,6 +29,34 @@ class OpenCommercial {
     inputLine = keyboard.readLine();
 
     /* Replace this comment with your solution.  */
+    String address = "http://www." + inputLine + ".com";
+    // address = "http://www.cs.berkeley.edu/~jrs/61bf06/hw/hw1/OpenCommercial.java";
+    System.out.println(address);
+
+    String[] firstFiveLines = new String[5];
+    URL aUrl = new URL(address);
+    // call the URL's openStream() method to get a stream
+    BufferedReader urlContent = new BufferedReader(new InputStreamReader(aUrl.openStream()));
+    // initialize a string variable
+    String oneInputLine;
+    // counter
+    int i = 0;
+    // System.out.println(firstFiveLines.length);
+    // Read URL contents one line a time, and store that line in the string variable
+    while((oneInputLine = urlContent.readLine()) != null && i < firstFiveLines.length) {
+	// System.out.println("====");
+	firstFiveLines[i] = oneInputLine + i;
+        // System.out.println(firstFiveLines[i] + i);
+	i++;
+        // System.out.println(oneInputLine + i);
+	// System.out.println(Arrays.toString(firstFiveLines));
+    }
+
+    // System.out.println(Arrays.toString(firstFiveLines));
+    // print in reverse order
+    for (int j = firstFiveLines.length - 1; j >= 0; j--) {
+        System.out.println(firstFiveLines[j]); 
+    }
 
   }
 }
